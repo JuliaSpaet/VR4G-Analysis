4de66f3f37edcc087a353dde86d7dcd905eaecd8
Julia Spaet <jlys3@cam.ac.uk>
2018-11-13 17:41:20 +1100

    Code still doesn't loop through locations

--- Temperature.R
+++ Temperature.R
@@ -24,7 +24,6 @@ timebin_location_date <- Study_Period %>%
   map(function(x) { timebin %>% mutate(date=x$date) }) %>%
   bind_rows()
 
-
 #load temperature csv file including VR4G and Hobo data
 temperature <- read.csv(file.choose())
 
@@ -43,23 +42,8 @@ Temperature_final <- timebin_location_date %>%
   left_join(temperature2,timebin_location_date,
             by= c('date' = 'date','location' = 'location', 'timebin' = 'timebin'))
 
-#I am confused why Temperature_final contains 333 559 rows. When I multiply 24 timebins 
-# with 19 locations and 730 days I get 332 880. Any idea what might be going on here?
 
-# I am trying to add a column with interpolated temperature values. However,
-# the below code runs it through the entire column, not considering the different locations
-Temperature_final$inter_temp<-na.interpolation(Temperature_final$temperature)
 
-#I have hence tried to use the following but I am not sure I understand all functions in purrr
-# I get an error message saying "Error in bind_rows_(x, .id) :
-#Argument 4 must be length 17520, not 17524 "
-
-inter<- function (x) {x$inter_temp<-na.interpolation(x$temperature)}
-
-Temp_final <- Temperature_final %>%
-  split(.$location) %>%
-  map(inter)%>%
-  bind_rows()
   
 
 
