261622652b3efd2e959e52fbfa2bc12c084cb0fd
Chris Knox <chris.knox@nzherald.co.nz>
2018-12-02 21:26:34 +1300

    suggestions of cleaner code

--- moon_phase.R
+++ moon_phase.R
@@ -24,11 +24,15 @@ angle<-read_csv(file.choose())
 #delete X1 column
 angle$X1<-NULL
 
+
+
+
 #to add a column that only contains the decimal part of the phase number
 angle$moonphase<-angle$phase-floor(angle$phase)
 
 #delete phase column
 angle$phase<-NULL
+
 
 # Insert new column with New South Wales time 
 ##PROBLEM: Twice a year when time gets switched from daylight savings to standard
@@ -57,3 +61,39 @@ angle3$nswtime<-as.Date(sub('(\\d+/\\d+/\\d+) .*','\\1',angle3$nswtime),'%Y-%m-%
 colnames(angle3)[colnames(angle3)=="nswtime"] <- "date"
 
 write.csv(angle3, file="moonphase.csv")
+
+
+# I would create angle like this
+# it does almost everything that you have done above
+
+# you can use as.XXXX to convert all kinds of things rather than have to save and load :)
+# it is also better to use for converting timezones and for grabbing the date portion
+# of a time
+# The tidyverse select function is cleaner to use than having to set stuff to NULL
+# it just lets you pull out what you want.
+
+# I'm not sure if this makes sense - since I haven't converted the
+# time into AEDT the timebins probably don't make sense.
+# You will either have to have timebins in GMT and convert them as well
+# or create the time bins using c(c(10:23),c(0:9))
+
+
+angle <- moonAngle(t, -30.3, 153.1, useRefraction=TRUE) %>% 
+  as.tibble() %>%
+  mutate(moonphase=phase-floor(phase)) %>%
+  filter(time > as_datetime("2016-12-01 00:00:00", tz='Australia/NSW'),
+         time <= as_datetime("2018-12-01 00:00:00", tz='Australia/NSW')) %>%
+  mutate(timebins=rep(0:23, times=730, each=1), date=as.Date(time)) %>%
+  # OR if you want the bins based arrounf GMT - 10
+  #  mutate(timebins=rep(c(c(10:23),c(0:9)), times=730, each=1), date=as.Date(time)) %>%
+  select(date,timebins,moonphase)
+
+# if the chaining confusing the same thing can be written as
+angle1 <- as.tibble(moonAngle(t, -30.3, 153.1, useRefraction=TRUE))
+angle2 <- mutate(angle1, moonphase=phase-floor(phase))
+angle3 <- filter(angle2, time > as_datetime("2016-12-01 00:00:00", tz='Australia/NSW'),
+                         time <= as_datetime("2018-12-01 00:00:00", tz='Australia/NSW'))
+angle4 <- mutate(angle3, timebins=rep(0:23, times=730, each=1),
+                         date=as.Date(time))
+angle <- select(angle4, date, timebins, moonphase)
+
